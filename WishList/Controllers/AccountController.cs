088ad5c423d3f8c9b8e73c07f3f07a0d37d023e8
Darren Nellis <darren.nellis@existec.com>
2021-09-24 14:49:03 +0100

    added post and get register methods

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+using WishList.Models.AccountViewModels;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View("/Register");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("/Register", register);
+            }
+
+            var result = await _userManager.CreateAsync(
+                new ApplicationUser()
+                {
+                    Email = register.Email,
+                }, 
+                register.Password
+            );
+
+            if (!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError(nameof(register.Password), error.Description);
+                }
+            }
+
+            return RedirectToAction("index", "Home");
+        }
+    }
+}
---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
